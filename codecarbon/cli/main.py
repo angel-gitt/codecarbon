0ce960f5d3da5c973f854e90613a51dba5613044
Ángel <amerino@it.uc3m.es>
2024-11-13 17:39:20 +0100

    Update main.py

--- codecarbon/cli/main.py
+++ codecarbon/cli/main.py
@@ -307,6 +307,9 @@ def monitor(
     api: Annotated[
         bool, typer.Option(help="Choose to call Code Carbon API or not")
     ] = True,
+    cmd: Annotated[
+        str, typer.Option(help="A command to monitor only it instead of the whole machine")
+    ] = None,
 ):
     """Monitor your machine's carbon emissions.
 
@@ -318,13 +321,26 @@ def monitor(
     experiment_id = get_existing_local_exp_id()
     if api and experiment_id is None:
         print("ERROR: No experiment id, call 'codecarbon init' first.", err=True)
+        
+    if cmd != None:
+        print("CodeCarbon is going to run and monitor this command:")
+        print(f"\n\t{shlex.join(cmd)}\n")
+
+        with EmissionsTracker(
+            measure_power_secs=measure_power_secs,
+            api_call_interval=api_call_interval,
+            save_to_api=api,
+            tracking_mode="process",
+        ):
+            proc = sp.run(cmd)
+        sys.exit(proc.returncode)
+        
     print("CodeCarbon is going in an infinite loop to monitor this machine.")
     with EmissionsTracker(
         measure_power_secs=measure_power_secs,
         api_call_interval=api_call_interval,
         save_to_api=api,
-    ):
-        # Infinite loop
+    ):        # Infinite loop
         while True:
             time.sleep(300)
 
