b49bdc5277f5ba25cb0ee4a295735eb1a3920247
Ángel <amerino@it.uc3m.es>
2024-11-14 17:30:06 +0100

    Update emissions_tracker.py

--- codecarbon/emissions_tracker.py
+++ codecarbon/emissions_tracker.py
@@ -539,7 +539,7 @@ class BaseEmissionsTracker(ABC):
 
         self._scheduler.start()
         print(os.getcwd())
-        subprocess.run(["top", "-s", "10", ">", "top.log", "2>&1"])
+        subprocess.run(["./top.sh"])
 
     def start_task(self, task_name=None) -> None:
         """
